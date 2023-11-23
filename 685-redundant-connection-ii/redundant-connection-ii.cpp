e412865624ebac25b5b493e0668c4e6eabeccf03
이문빈 <3412mb@gmail.com>
2023-11-23 16:20:52 +0900

    Time: 7 ms (63.73%) | Memory: 9.9 MB (58.38%) - LeetSync

--- 685-redundant-connection-ii/redundant-connection-ii.cpp
+++ 685-redundant-connection-ii/redundant-connection-ii.cpp
@@ -1,67 +1,50 @@
 class Solution {
 public:
-    vector<int> parents;
-    vector<vector<int>> indegrees;
+    vector<int> parents, roots;
 
     int find(int x) {
-        if (x == parents[x]) return x;
-        return parents[x] = find(parents[x]);
-    }
+        if (x == roots[x]) return x;
 
-    void merge(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a > b) swap(a, b);
-        parents[a] = b;
-    }
-
-    bool isCycle(int cur, int target, bool first) {
-        if (!first && cur == target) return true;
-
-        for (int indegree : indegrees[cur]) {
-            if (isCycle(indegree, target, false)) return true;
-        }
-
-        return false;
+        return roots[x] = find(roots[x]);
     }
 
     vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        parents.resize(n + 1);
-        iota(parents.begin(), parents.end(), 0);
-        indegrees.resize(n + 1);
+        parents.resize(n + 1, -1);
+        roots.resize(n + 1);
+        iota(roots.begin(), roots.end(), 0);
 
-        int doubleIndegree = -1;
+        int hasDoubleParents = -1;
+        array<int, 2> doubleParents;
         vector<int> cycle;
 
         for (auto &edge : edges) {
             int u = edge[0];
             int v = edge[1];
 
-            indegrees[v].emplace_back(u);
-
-            if (indegrees[v].size() == 2) {
-                doubleIndegree = v;
+            // parent가 2개인 경우
+            if (parents[v] != -1){
+                hasDoubleParents = v;
+                doubleParents = {parents[v], u};
+                parents[v] = -1;
+                continue;
             }
 
+            parents[v] = u;
+
+            // 사이클인 경우
             if (find(u) == find(v)) {
                 cycle = {u, v};
-            } else {
-                merge(u, v);
+                continue;
             }
+            
+            roots[v] = u;
         }
 
-        if (doubleIndegree == -1) return cycle;
+        if (hasDoubleParents == -1) return cycle;
 
-        int a = indegrees[doubleIndegree][0];
-        int b = indegrees[doubleIndegree][1];
+        if (cycle.empty()) return {doubleParents[1], hasDoubleParents};
 
-        if (cycle.empty()) return {b, doubleIndegree};
-
-        for (int i : {b, a}) {
-            if (isCycle(i, doubleIndegree, true)) return {i, doubleIndegree};
-        }
-
-        return {b, doubleIndegree};
+        return {doubleParents[0], hasDoubleParents};
     }
 };
